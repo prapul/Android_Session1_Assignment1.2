00261b42bbdfd60e3eff1440e85c39fac8554ba9
prapul <prapulpoonacha@gmail.com>
2016-12-20 10:51:46 +0530

    AndroidSession1_Assignment1.2
    Operations on different types of operators

---
+++ Assignment_1.java
@@ -0,0 +1,23 @@
+package prapul;
+
+public class Assignment1 {
+
+		public static void main(String[] args) {
+			int a,b,c,d,e;
+		    a=20;
+			b=10;
+			b= (a--)-(--a);
+			System.out.println("The value of b after 'a-- - --a' is "+b);
+			c=a--;
+			System.out.println("The value of c after 'a--' is "+c);
+			d=a>>2;
+			System.out.println("The value of d after 'a>>2' is "+d);
+			e= a&b;
+			System.out.println("The value of e after 'a&b' is "+e);
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+
